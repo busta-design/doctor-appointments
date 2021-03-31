c64b329c69c9e8b5f0456f6397f2572e14a7b086
elpep <andresbusta4444@gmail.com>
2021-03-30 20:57:13 -0400

    Complete Project

---
+++ src/Main.java
@@ -0,0 +1,57 @@
+import model.*;
+import ui.UIMenu;
+
+import java.util.Date;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        UIMenu.showMenu();
+//        Doctor myDoctor = new Doctor("Anahi Salgado", "anahi@anahi");
+//        myDoctor.addAvailableAppointment(new Date(), "4pm");
+//        myDoctor.addAvailableAppointment(new Date(), "10am");
+//        myDoctor.addAvailableAppointment(new Date(), "1pm");
+
+//        System.out.println(myDoctor);
+//        System.out.println();
+//        System.out.println();
+//        User user = new Doctor("Anahi", "ana@ana.com");
+//        user.showDataUser();
+//        User userPa = new Patient("Anahi", "ana@ana.com");
+//        userPa.showDataUser();
+//
+//        User user1 = new User("Andres", "andres@andres.com") {
+//            @Override
+//            public void showDataUser() {
+//                System.out.println("Doctor");
+//                System.out.println("Hospital: Cruz Verde");
+//                System.out.println("Departamento: Geriatria");
+//            }
+//        };
+//        user1.showDataUser();
+//
+//        ISchedulable iSchedulable = new ISchedulable() {
+//            @Override
+//            public void schedule(Date date, String time) {
+//
+//            }
+//        };
+//
+//        ISchedulable iSchedulable1 = new AppointmentDoctor();
+//        iSchedulable1.schedule(new Date(), "hola");
+
+
+//        for (model.Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()) {
+//            System.out.println(aA.getDate() + " ---- " + aA.getTime() );
+//        }
+
+//        System.out.println();
+//        System.out.println();
+//
+//        Patient patient = new Patient("Alejandra", "alejandra@gmail.com");
+//        System.out.println(patient);
+
+
+    }
+}
